44b099cfa37b528f7be58c4d6d1d279d8c0f168c
vip-you <123567019+vip-you@users.noreply.github.com>
2023-07-29 22:55:07 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,8 +5,8 @@ self.addEventListener('install', event => {
             return cache.addAll([
                 // List of files to be cached
                 'index.html',
-                'https://vip-you.github.io/update/icon-192x192.png',
-                'https://vip-you.github.io/update/icon-512x512.png',
+                'https://vip-you.github.io/update/icon.png',
+                'https://vip-you.github.io/update/icon.png',
                 // Add other assets here, e.g., CSS, JS, images, etc.
             ]);
         })
