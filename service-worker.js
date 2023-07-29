4e2505ab86d7eed3f4492a48f1db3d0743991ab8
vip-you <123567019+vip-you@users.noreply.github.com>
2023-07-29 22:12:55 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+// Service worker installation and caching
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('my-web-app-v1').then(cache => {
+            return cache.addAll([
+                // List of files to be cached
+                'index.html',
+                'http://rajdeepsharma.tk/icon-192x192.png',
+                'http://rajdeepsharma.tk/icon-512x512.png',
+                // Add other assets here, e.g., CSS, JS, images, etc.
+            ]);
+        })
+    );
+});
+
+// Service worker fetch event (cache-first strategy)
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
